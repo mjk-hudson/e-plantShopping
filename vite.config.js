8d16d586c1ebabd316034ec5e433903b5b5d08c9
mjk-hudson <majik.hudson@icloud.com>
2025-10-27 23:15:22 -0400

    Final Commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/blank",
+  base: "/e-plantShopping/",
   plugins: [react()],
   server: {
     open: true,  // Automatically opens http://localhost:5173 in the browser
